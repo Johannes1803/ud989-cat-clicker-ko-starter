6f045f83b1e2747984a6f53d08d8057961da45cc
Johannes1803 <Johannes.Birk11@web.de>
2016-05-15 16:59:20 +0200

    Change level of cat

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.imggAttribution = ko.observable('....');
+	this.level = ko.computed(function() {
+		console.log(this.clickCount);
+		if (this.clickCount() < 11) {
+			return "Rookie"
+		} else {
+			return "Expert"
+		}
+	}, this);
+
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+
+}
+
+ko.applyBindings(new ViewModel())
